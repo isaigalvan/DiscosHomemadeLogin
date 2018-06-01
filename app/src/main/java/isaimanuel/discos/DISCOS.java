110a45aff0be98fa546a0a769f4857a0ff0e8a00
GoreMazter <Lucas.2451@hotmail.com>
2018-06-01 12:39:33 -0500

    prueba push, se añadió referencia a usuario

--- app/src/main/java/isaimanuel/discos/DISCOS.java
+++ app/src/main/java/isaimanuel/discos/DISCOS.java
@@ -2,12 +2,14 @@ package isaimanuel.discos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DISCOS extends AppCompatActivity {
-
+    EditText usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discos);
+        usuario=(EditText)findViewById(R.id.user);
     }
 }
